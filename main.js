36cccc0a42152797dad406c818e8f44a453133f6
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-03-29 01:40:33 +0600

    #2 Реализовал добавление нового поста

--- main.js
+++ main.js
@@ -21,23 +21,40 @@ let getProfile = async function () {
 }
 let getPosts = async function () {
     let posts = await makeRequest(`${baseUrl}/posts`)
-    posts = posts.map(post => ({...post, datetime: new Date(post.datetime)}))
-    posts.sort((a, b) => b.datetime - a.datetime)
-    for (let post of posts) {
+    let newPosts = posts.map(post => ({...post, datetime: new Date(post.datetime)}))
+    let postBlock = $('#posts-block')
+    newPosts.sort((a, b) => b.datetime - a.datetime)
+    postBlock.empty()
+    for (let post of newPosts) {
         let cardTitle = $('<p>').addClass('card-title').text(`${post.user.firstName} ${post.user.lastName}`)
         let cardDate = $('<p>').addClass('card-text').append($('<small>').text(`${post.datetime.toLocaleString()}`))
         let cardText = $('<h4>').addClass('card-text').text(post.message)
         let cardBody = $('<div>').addClass('card-body').append(cardTitle, cardText, cardDate)
         let card = $('<div>').addClass('card').append(cardBody)
-        $('#posts-block').append(card)
+        postBlock.append(card)
     }
 }
+let addPost = async function (event) {
+    try {
+        await makeRequest(`${baseUrl}/posts`, $(event.currentTarget).serialize(), 'POST')
+        event.currentTarget.reset()
+    }
+    catch (error) {
+        console.log(error)
+    }
+
+}
+
 let onLoad = async function () {
     let user = await getProfile()
     await getPosts()
-    let intervalId = setInterval(function(){getPosts()}, 4000);
+    let intervalId = setInterval(async function(){await getPosts()}, 4000);
     $('.profile-block > h3').text(`${user.firstName} ${user.lastName}`)
 }
 $(document).ready(function (){
-    onLoad().then();
+    $('#post-form').submit(function(event){
+        event.preventDefault()
+        return addPost(event).then(getPosts)
+    });
+    return onLoad()
 })
